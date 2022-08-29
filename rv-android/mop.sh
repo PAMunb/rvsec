820080721032451bdef3bd589f73b425a8095791
Pedro Costa <phtcosta@gmail.com>
2022-08-29 11:01:59 -0300

    configuring javamop

--- rv-android/mop.sh
+++ rv-android/mop.sh
@@ -17,29 +17,24 @@ JAVAMOP_HOME=../javamop
 RV_MONITOR_HOME=../rv-monitor
 
 ANDROID_PLATFORM=android-17
-ANDROID_SDK_HOME=/home/pedro/Android/Sdk
+ANDROID_SDK_HOME=$ANDROID_HOME
 ANDROID_PLATFORM_LIB=$ANDROID_SDK_HOME/platforms/$ANDROID_PLATFORM
 
 
 # Set up output directories, removing old files
 find $2 -name "*.java" -exec rm -Rf {} \;
-find $2 -name "*.rvm" -exec rm -Rf {} \;
-find $2 -name "*.aj" -exec rm -Rf {} \;
+#find $2 -name "*.rvm" -exec rm -Rf {} \;
+#find $2 -name "*.aj" -exec rm -Rf {} \;
 rm -rf out tmp rvm_tmp lib_tmp
 mkdir out
 mkdir tmp
 mkdir rvm_tmp
 mkdir lib_tmp
 
-
 # Copy dependency JARs to 'lib_tmp' folder 
 mvn clean compile
 CLASSPATH=$ANDROID_PLATFORM_LIB/android.jar:$(for i in lib_tmp/*.jar ; do echo -n $i: ; done):.
-#CLASSPATH=/home/pedro/Android/Sdk/platforms/android-17/android.jar:lib_tmp/aspectjrt.jar:lib_tmp/aspectjtools.jar:lib_tmp/aspectjweaver.jar:lib_tmp/rv-monitor-rt.jar:lib_tmp/rv-monitor.jar:lib_tmp/rvsec.jar:.
 echo "CLASSPATH=$CLASSPATH"
-#/home/pedro/desenvolvimento/aplicativos/aspectj-1.8.1/lib/aspectjrt.jar:
-
-
 
 # Convert APK to Jar (with Java bytecode), verify output Jar
 echo "[+] Extracting Java classes to JAR in tmp/"
@@ -59,13 +54,6 @@ echo "[+] Executing RV-Monitor"
 $RV_MONITOR_HOME/bin/rv-monitor -s -merge -d $2 $2/*.rvm
 
 
-# Move all Java monitor classes into their final package namespace directory
-python lib/fix_java_packages.py
-if [ "$?" = 1 ] ; then
-    # Unable to resolve Java package
-    exit
-fi
-
 
 cp $2/*.java rvm_tmp/.
 cp $2/*.aj rvm_tmp/.
@@ -76,28 +64,29 @@ cp $2/*.aj rvm_tmp/.
 cp -rf rvm_tmp/ tmp/
 rm -rf rvm_tmp/*
 
+
+#unzip lib_tmp/rv-monitor-rt.jar rvm_tmp/.
+#unzip lib_tmp/rvsec.jar -d tmp
+
+
 # Instrument application with monitor classes
-#ajc -cp $CLASSPATH -inpath tmp -d tmp -source 1.8 -sourceroots $2
-#ajc -cp $CLASSPATH -inpath tmp -d tmp -source 1.8 -sourceroots $2
-#if [ "$?" = 1 ] ; then
-#    echo "AspectJ has encountered a fatal error and needs to close. Dying!"
-#    exit
-#fi
-
-#ajc -cp /home/pedro/Android/Sdk/platforms/android-17/android.jar:lib_tmp/aspectjrt.jar:lib_tmp/aspectjtools.jar:lib_tmp/aspectjweaver.jar:lib_tmp/cfg.jar:lib_tmp/ere.jar:lib_tmp/fsm.jar:lib_tmp/guava.jar:lib_tmp/logicrepository.jar:lib_tmp/ltl.jar:lib_tmp/pda.jar:lib_tmp/po.jar:lib_tmp/ptcaret.jar:lib_tmp/ptltl.jar:lib_tmp/rv-monitor-rt.jar:lib_tmp/rv-monitor.jar:lib_tmp/rvsec.jar:lib_tmp/scala-library.jar:lib_tmp/scala-parser-combinators_2.11.jar:lib_tmp/srs.jar:lib_tmp/tfsm.jar:mop:tmp:. -inpath tmp -d tmp -source 1.8 -sourceroots mop
-ajc -cp /home/pedro/Android/Sdk/platforms/android-17/android.jar:lib_tmp/aspectjrt.jar:lib_tmp/aspectjtools.jar:lib_tmp/aspectjweaver.jar:lib_tmp/cfg.jar:lib_tmp/ere.jar:lib_tmp/fsm.jar:lib_tmp/guava.jar:lib_tmp/logicrepository.jar:lib_tmp/ltl.jar:lib_tmp/pda.jar:lib_tmp/po.jar:lib_tmp/ptcaret.jar:lib_tmp/ptltl.jar:lib_tmp/rv-monitor-rt.jar:lib_tmp/rv-monitor.jar:lib_tmp/rvsec.jar:lib_tmp/scala-library.jar:lib_tmp/scala-parser-combinators_2.11.jar:lib_tmp/srs.jar:lib_tmp/tfsm.jar:mop:tmp:. -Xlint:ignore -showWeaveInfo -inpath tmp -d tmp -source 1.8 -sourceroots mop
-
-
-
+echo "[+] Instrumenting"
+ajc -Xmx10240m -cp $CLASSPATH:mop:tmp:. -Xlint:ignore -showWeaveInfo -inpath tmp -d tmp -source 1.8 -sourceroots mop
+if [ "$?" = 1 ] ; then
+    echo "AspectJ has encountered a fatal error and needs to close. Dying!"
+    exit
+fi
 
 
 # Extract RV-Monitor support classes
 cp lib_tmp/rv-monitor-rt.jar rvm_tmp/.
+cp lib_tmp/rvsec.jar rvm_tmp/.
 cd rvm_tmp
 jar xf rv-monitor-rt.jar
+#jar xf rvsec.jar
 
 # Remove rvmonitorrt's manifest and the temporarily copied Jar + property files
-rm -rf META-INF rv-monitor-rt.jar
+rm -rf META-INF rv-monitor-rt.jar rvsec.jar
 cd ..
 
 # Merge RV-Monitor support classes
--- rv-android/run.sh
+++ rv-android/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-APK=osmtracker.apk
+#APK=osmtracker.apk
+APK=media.apk
 
 ./mop.sh $APK mop
