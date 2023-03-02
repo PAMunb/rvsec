9c8dbb98bf747a827eb1c5857fdef38a3f69badb
Pedro Costa <phtcosta@gmail.com>
2023-03-02 09:41:30 -0300

    including stacktrace

--- crylogger/scripts/deltas/libcore/ojluni/src/main/java/java/security/CRYLogger.java
+++ crylogger/scripts/deltas/libcore/ojluni/src/main/java/java/security/CRYLogger.java
@@ -10,69 +10,74 @@ import java.io.FileOutputStream;
  */
 public class CRYLogger {
 
-    private static FileOutputStream stream;
+	private static FileOutputStream stream;
 
-    /**
-     *  Write the specified string in the log file.
-     *
-     *  @param string the string to be written.
-     */
-    synchronized static public void write(String string) {
+	/**
+	 * Write the specified string in the log file.
+	 *
+	 * @param string the string to be written.
+	 */
+	synchronized static public void write(String string) {
 
-        try {
+		try {
 
-            if (stream == null) {
+			if (stream == null) {
 
-                String filePath = System.getProperty("java.io.tmpdir");
-                String fileName = filePath + "/application.cryptolog";
-                String startMsg = "[CRYLOGGER] start logging here\n";
-                stream = new FileOutputStream(fileName, true);
-                stream.write(startMsg.getBytes());
-            }
+				String filePath = System.getProperty("java.io.tmpdir");
+				String fileName = filePath + "/application.cryptolog";
+				String startMsg = "[CRYLOGGER] start logging here\n";
+				stream = new FileOutputStream(fileName, true);
+				stream.write(startMsg.getBytes());
+			}
 
-            stream.write(string.getBytes());
+			StringBuilder sb = new StringBuilder("\n"+string+"::");
+			StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+//			int max = (stackTrace.length > 20) ? 20 : stackTrace.length;
+			for (int i = stackTrace.length-1; i > 0; i--) {
+				sb.append(stackTrace[i].getClassName()+"::"+stackTrace[i].getMethodName()+" ### ");
+			}
 
-        } catch (Exception e) {
+		
+			stream.write(sb.toString().getBytes());
 
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
 
-    /**
-     *  Write the specified string and the byte array in the log file.
-     *
-     *  @param string the string to be written.
-     *  @param array the byte array to be written.
-     */
-    synchronized static public void write(String string, byte[] array) {
+			e.printStackTrace();
+		}
+	}
 
-        try {
+	/**
+	 * Write the specified string and the byte array in the log file.
+	 *
+	 * @param string the string to be written.
+	 * @param array  the byte array to be written.
+	 */
+	synchronized static public void write(String string, byte[] array) {
 
-            if (stream == null) {
+		try {
 
-                String filePath = System.getProperty("java.io.tmpdir");
-                String fileName = filePath + "/application.cryptolog";
-                String startMsg = "[CRYLogger] start logging here\n";
-                stream = new FileOutputStream(fileName, true);
-                stream.write(startMsg.getBytes());
-            }
+			if (stream == null) {
 
-            stream.write(string.getBytes());
+				String filePath = System.getProperty("java.io.tmpdir");
+				String fileName = filePath + "/application.cryptolog";
+				String startMsg = "[CRYLogger] start logging here\n";
+				stream = new FileOutputStream(fileName, true);
+				stream.write(startMsg.getBytes());
+			}
 
-            if (array != null)
-            {
-                for (byte b: array)
-                    stream.write((String.format("%02x", b)).getBytes());
-                stream.write("\n".getBytes());
-            }
-            else
-            {
-                stream.write("null\n".getBytes());
-            }
+			stream.write(string.getBytes());
 
-        } catch (Exception e) {
+			if (array != null) {
+				for (byte b : array)
+					stream.write((String.format("%02x", b)).getBytes());
+				stream.write("\n".getBytes());
+			} else {
+				stream.write("null\n".getBytes());
+			}
 
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
 }
