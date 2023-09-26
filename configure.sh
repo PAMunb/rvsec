70f3989885d291102b3a48c54131e7b43ef5df8f
Jonata Pastro <jtpastro@gmail.com>
2023-09-26 09:40:53 -0300

    Moving configure.sh back to repo root so that installation instructions are correct

---
+++ configure.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -z "$ASPECTJ_HOME" ]; then
+    echo "[Error] No ASPECTJ_HOME environment variable found"
+    exit 1
+fi
+
+if [ -z "$M2_REPO" ]; then
+    M2_REPO=${HOME}/.m2/repository
+fi
+
+RV_MONITOR_REPO=${M2_REPO}/br/unb/cic/rvmonitor
+RV_MONITOR_VERSION=0.5.0-SNAPSHOT
+
+CLASSPATH=${RV_MONITOR_REPO}/rv-monitor/${RV_MONITOR_VERSION}/rv-monitor-${RV_MONITOR_VERSION}.jar:${RV_MONITOR_REPO}/rv-monitor-rt/${RV_MONITOR_VERSION}/rv-monitor-rt-${RV_MONITOR_VERSION}.jar:${ASPECTJ_HOME}/lib/aspectjrt.jar:${ASPECTJ_HOME}/lib/aspectjweaver.jar:${ASPECTJ_HOME}/lib/aspectjtools.jar    
+
+export CLASSPATH
+
+echo $CLASSPATH
+
+echo "CLASSPATH environment variable set."
+
