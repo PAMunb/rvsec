f75f2c0d0af6b7add3d4197e53448ec2ed51d099
pedro <pedro@canario>
2023-11-22 13:03:33 -0300

    updating scripts

--- rv-android/clear.sh
+++ rv-android/clear.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# WARNING: This script removes several directories, including results. Please check before using
+# WARNING: This script removes several directories(, including results). Please check before using
 
 OUT_DIR="out"            # out dir (for instrumented apk)
 LIB_TMP="lib_tmp"        # libs dir (maven dependencies)
+MOP_OUT="mop_out"
 TMP_DIR="tmp"
 RVM_TMP_DIR="rvm_tmp"
 RESULTS_DIR="results"
 
 echo "[+] Cleaning ..."
-rm -rf $TMP_DIR $RVM_TMP_DIR $LIB_TMP $OUT_DIR #$RESULTS_DIR
+rm -rf __pycache__ $TMP_DIR $RVM_TMP_DIR $LIB_TMP $OUT_DIR $MOP_OUT #$RESULTS_DIR
 rm -v *.dex
 rm ajcore*.txt
