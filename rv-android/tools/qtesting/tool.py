6d50858a74acbc8723cdb19c1af6ae2b5deebcdf
Jonata Pastro <jtpastro@gmail.com>
2024-06-03 10:11:40 -0300

    Generating qtesting configuration file at runtime

--- rv-android/tools/qtesting/tool.py
+++ rv-android/tools/qtesting/tool.py
@@ -2,23 +2,30 @@ from app import App
 from commands.command import Command
 
 from ..tool_spec import AbstractTool
-import os, fileinput, re
+import os, re
 from settings import WORKING_DIR, INSTRUMENTED_DIR
 
 class ToolSpec(AbstractTool):
     def __init__(self):
-        super(ToolSpec, self).__init__("qlearning", """ qlearning """, 'main.py')
+        super(ToolSpec, self).__init__("qtesting", """ qtesting """, 'main.py')
 
     def execute_tool_specific_logic(self, app: App, timeout: int, log_file: str):
-        qlearning_entrypoint = os.path.join(WORKING_DIR, 'tools', 'qlearning', 'src', 'main.py')
-        config_file = os.path.join(WORKING_DIR, 'tools', 'qlearning', 'src', 'conf_template.txt')
-        with fileinput.input(config_file, inplace=True) as f:
-            for line in f:
-                print(re.sub(r"(APK_NAME = ).+", r"\1{0}".format(app.path)), end='')
+        qtesting_entrypoint = os.path.join(WORKING_DIR, 'tools', 'qtesting', 'src', 'main.py')
+        config_file = os.path.join(WORKING_DIR, 'tools', 'qtesting', 'src', 'conf.txt')
+        with open(config_file, "w") as f:
+            f.write("""
+            [Path]
+            Benchmark = 
+            APK_NAME = {0}
+            [Setting]
+            DEVICE_ID = emulator-5554
+            TIME_LIMIT = {1}
+            TEST_INDEX=1""".format(app.path,timeout))
 
-        with open(log_file, 'wb') as qlearning_trace:
-            exec_cmd = Command('python3', [
-                '{}'.format(qlearning_entrypoint),
-                '-r {0}'.format(config_file)
+        with open(log_file, 'wb') as qtesting_trace:
+            exec_cmd = Command('{}'.format(qtesting_entrypoint), [
+                '-r',
+                '{0}'.format(config_file)
             ], timeout=timeout)
-            exec_cmd.invoke(stdout=qlearning_trace)
+            print(exec_cmd.args)
+            exec_cmd.invoke(stdout=qtesting_trace)
