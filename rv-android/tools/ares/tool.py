ea3e5a07433d74fb2aa4489520054d4e4319c24a
Pedro Costa <phtcosta@gmail.com>
2024-07-16 11:34:50 -0300

    adjusting timeout

--- rv-android/tools/ares/tool.py
+++ rv-android/tools/ares/tool.py
@@ -5,18 +5,20 @@ from ..tool_spec import AbstractTool
 import os, re
 from settings import WORKING_DIR, INSTRUMENTED_DIR
 
+
 class ToolSpec(AbstractTool):
     def __init__(self):
         super(ToolSpec, self).__init__("ares", """ ares """, 'run_ares.sh')
 
     def execute_tool_specific_logic(self, app: App, timeout: int, log_file: str):
         ares_entrypoint = os.path.join(WORKING_DIR, 'tools', 'ares', 'run_ares.sh')
-        
+
+        timeout_in_minutes = int(timeout/60)
+
         with open(log_file, 'wb') as ares_trace:
-            exec_cmd = Command(
-                '{}'.format(ares_entrypoint), 
-                [app.path,
-                 'emulator-5554',
-                  timeout]
-            )
+            exec_cmd = Command('{}'.format(ares_entrypoint),  [
+                app.path,
+                'emulator-5554',
+                str(timeout_in_minutes)
+            ], timeout)
             exec_cmd.invoke(stdout=ares_trace)
