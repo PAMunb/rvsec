0f60a56daed0f37c2f7c9191b0535af96245763f
Pedro Costa <phtcosta@gmail.com>
2024-09-18 09:23:24 -0300

    creating module for APK information

---
+++ rvsec/rvsec-android/rvsec-apk/src/main/java/br/unb/cic/rvsec/apk/model/ActivityInfo.java
@@ -0,0 +1,65 @@
+package br.unb.cic.rvsec.apk.model;
+
+import java.util.Objects;
+
+public class ActivityInfo {
+	private String name, shortName, packageName;
+	private boolean isMain = false;
+	private String layoutFileName;
+
+	public ActivityInfo(String name, boolean isMain) {
+		setName(name);
+		this.isMain = isMain;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
+		this.packageName = name.substring(0, name.lastIndexOf('.'));
+		this.shortName = name.substring(name.lastIndexOf('.') + 1);
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+
+	public String getPackageName() {
+		return packageName;
+	}
+
+	public boolean isMain() {
+		return isMain;
+	}
+
+	public String getLayoutFileName() {
+		return layoutFileName;
+	}
+
+	public void setLayoutFileName(String layoutFileName) {
+		this.layoutFileName = layoutFileName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(isMain, layoutFileName, name, packageName, shortName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if ((obj == null) || (getClass() != obj.getClass()))
+			return false;
+		ActivityInfo other = (ActivityInfo) obj;
+		return isMain == other.isMain && Objects.equals(layoutFileName, other.layoutFileName) && Objects.equals(name, other.name) && Objects.equals(packageName, other.packageName) && Objects.equals(shortName, other.shortName);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("ActivityInfo [name=%s, isMain=%s, layoutFileName=%s]", name, isMain, layoutFileName);
+	}
+
+}
---
+++ rvsec/rvsec-android/rvsec-apk/src/main/java/br/unb/cic/rvsec/apk/util/StringUtil.java
@@ -0,0 +1,57 @@
+package br.unb.cic.rvsec.apk.util;
+
+public class StringUtil {
+	
+//    public static String convertToAct(String classStr){
+//    	System.out.println("convertToAct="+classStr);
+//        int len = classStr.length();
+//        String str = classStr.substring(8, len - 2);
+//        return str.replace('/', '.');
+//    }
+
+    public static String convertToLabel(String apkPath){
+    	System.out.println("convertToLabel: "+apkPath);
+        String[] nameArray = null;
+        if(apkPath.contains("/")){
+            nameArray = apkPath.split("/");
+        }else{
+            nameArray = apkPath.split("\\\\");
+        }
+        String appFullName = nameArray[nameArray.length - 1];
+        int length = appFullName.length();
+        return appFullName.substring(0, length - 4);
+    }
+    
+//    public static String longestCommonPrefix(List<String> strs) {
+//        if (strs == null || strs.size() == 0) {
+//            return "";
+//        }
+//
+//        String prefix = strs.get(0);
+//        for (int i = 1; i < strs.size(); i++) {
+//            while (!strs.get(i).startsWith(prefix)) {
+//                prefix = prefix.substring(0, prefix.length() - 1);
+//                if (prefix.isEmpty()){
+//                    return "";
+//                }
+//            }
+//        }
+//        return prefix;
+//    }
+//    
+//    
+//    public static boolean isHexadecimal(String strNum) {
+//        if (strNum == null) {
+//        	System.err.println("null");
+//            return false;
+//        }
+//        try {
+//            Integer.parseInt(strNum.substring(2), 16);
+//        } catch (NumberFormatException nfe) {
+//        	System.err.println("NumberFormatException: "+nfe.getMessage());
+//            return false;
+//        }
+//        return true;
+//    }
+    
+}
