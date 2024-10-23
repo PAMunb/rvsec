b1a518fc6bc6e67915630b0f85725359981d2432
Pedro Costa <phtcosta@gmail.com>
2024-10-23 14:22:06 -0300

    changing ranking method

--- rv-android/fdroid/select_apks_experiment02.py
+++ rv-android/fdroid/select_apks_experiment02.py
@@ -9,6 +9,9 @@ from csv import DictReader
 
 import requests
 from google_play_scraper import app
+import numpy as np
+from scikitmcda.constants import MAX, EnhancedAccuracy_
+from scikitmcda.topsis import TOPSIS
 
 URL = "url"
 REVIEWS = "reviews"
@@ -25,14 +28,14 @@ FILENAME = "filename"
 def execute(fdroid_file: str, results_file: str, out_file: str) -> None:
     apks = get_instrumented_apks(fdroid_file, results_file)
     print("Instrumented apks: {}".format(len(apks)))
-    get_app_id_from_fdroid_index(apks)
-    filter_apps_in_playstore(apks)
+    get_info_from_fdroid_index(apks)
+    get_info_from_playstore(apks)
+    select_apks(apks)
     save_results(apks, out_file)
     print("End of execution !!!")
 
 
 def get_instrumented_apks(fdroid_file: str, results_file: str) -> list[dict]:
-    # pega as linhas da planilha fdroid q contem apks que foram instrumentados no experimento 01 (results_file)
     print("Recovering instrumented apks ...")
     apks: list[dict] = []
     with open(fdroid_file, "r") as f:
@@ -41,24 +44,56 @@ def get_instrumented_apks(fdroid_file: str, results_file: str) -> list[dict]:
             for fdroid in DictReader(f):
                 apk_filename = fdroid["file"]
                 if apk_filename in result:
+                    summary = result[apk_filename]["summary"]
                     data_item = {FILENAME: apk_filename,
                                  NAME: fdroid["name"],
-                                 APP_ID: "",  # package name
+                                 APP_ID: get_app_id(apk_filename),  # package name
+                                 "selection_rank": 0, # mcda rank
+                                 "selection_score": 0.0,  # mcda score
+                                 "categories": fdroid["categories"],
+                                 "package": fdroid["package"],
                                  FDROID: False,  # (still) exists in fdroid
                                  PLAYSTORE: False,
+                                 "apk_date": 0,
+                                 "apk_date_str": "",
+                                 "last_update": 0,
+                                 "last_update_str": "",
                                  DOWNLOADS: 0,
                                  SCORE: 0.0,
                                  RATINGS: 0,
                                  REVIEWS: 0,
-                                 URL: ""}
+                                 "min_sdk": fdroid["min_sdk"],
+                                 "target_sdk": fdroid["target_sdk"],
+                                 "classes": summary["total_classes"],
+                                 "activities": summary["total_activities"],
+                                 "methods": summary["total_methods"],
+                                 "act_cov": summary["activities_coverage_avg"],
+                                 "method_cov": summary["method_coverage_avg"],
+                                 "rvsec_errors": len(result[apk_filename]["rvsec_errors"]),
+                                 "summary": "",
+                                 URL: "",
+                                 "source": fdroid["sourceCode"]}
                     apks.append(data_item)
     return apks
 
 
-def get_app_id_from_fdroid_index(instrumented_apks: list[dict]) -> None:
+def get_info_from_fdroid_index(instrumented_apks: list[dict]) -> None:
     print("Checking if the app still exists on fdroid ...")
     response = requests.get("https://f-droid.org/repo/index-v2.json")
     fdroid_index = response.json()
+    print("Number of applications in fdroid index: {}".format(len(fdroid_index["packages"])))
+
+    # cont = 0
+    # for apk in instrumented_apks:
+    #     app_id = apk["app_id"]
+    #     if app_id in fdroid_index["packages"]:
+    #         print(fdroid_index["packages"][app_id])
+    #         apk[FDROID] = True
+    #         # exit(1)
+    #     else:
+    #         cont += 1
+    #         # print(apk)
+    # print(cont)
 
     map_filename_to_app_id = {}
     for app_id in fdroid_index["packages"]:
@@ -66,24 +101,45 @@ def get_app_id_from_fdroid_index(instrumented_apks: list[dict]) -> None:
         for version in data["versions"]:
             filename = data["versions"][version]["file"]["name"]
             filename = filename[1:]
-            map_filename_to_app_id[filename] = app_id
+            apk_date = data["versions"][version]["added"]
+            apk_date_obj = datetime.datetime.fromtimestamp(apk_date / 1000)
+            map_filename_to_app_id[filename] = {"app_id": app_id,
+                                                "apk_date": apk_date,
+                                                "apk_date_str": apk_date_obj.strftime("%Y-%m-%d")}
 
     cont_not_exist = 0
     for apk in instrumented_apks:
         filename = apk[FILENAME]
         if filename in map_filename_to_app_id:
             apk[FDROID] = True
-            app_id = map_filename_to_app_id[filename]
+            app_info = map_filename_to_app_id[filename]
+            app_id = app_info["app_id"]
             apk[APP_ID] = app_id
-            last_updated = fdroid_index["packages"][app_id]["metadata"]["lastUpdated"]
+            apk["apk_date"] = app_info["apk_date"]
+            apk["apk_date_str"] = app_info["apk_date_str"]
+            metadata = fdroid_index["packages"][app_id]["metadata"]
+            apk["summary"] = get_summary(metadata)
+            last_updated = metadata["lastUpdated"]
             last_updated_date = datetime.datetime.fromtimestamp(last_updated / 1000)
-            apk["updated"] = last_updated_date.strftime("%Y-%m-%d")
+            apk["last_update"] = last_updated
+            apk["last_update_str"] = last_updated_date.strftime("%Y-%m-%d")
         else:
             cont_not_exist += 1
     print("Apps that no longer exist on fdroid: {}".format(cont_not_exist))
 
 
-def filter_apps_in_playstore(instrumented_apks: list[dict]) -> None:
+def get_summary(metadata):
+    summary = ""
+    if "en-US" in metadata["summary"]:
+        summary = metadata["summary"]["en-US"]
+    else:
+        key = next(iter(metadata["summary"]))
+        if key:
+            summary = metadata["summary"][key]
+    return summary
+
+
+def get_info_from_playstore(instrumented_apks: list[dict]) -> None:
     print("Recovering app data from playstore ...")
     cont = 0  # qtde que existe na playstore
     total_apks = len(instrumented_apks)
@@ -93,8 +149,8 @@ def filter_apps_in_playstore(instrumented_apks: list[dict]) -> None:
         status_pct = (cont_apk * 100) / total_apks
         print(f"\rProgress: {status_pct:.2f}%", end="")
 
-        if not apk[FDROID]:
-            continue
+        # if not apk[FDROID]:
+        #     continue
 
         app_id = apk[APP_ID]
         try:
@@ -105,20 +161,93 @@ def filter_apps_in_playstore(instrumented_apks: list[dict]) -> None:
             apk[RATINGS] = app_info["ratings"]
             apk[REVIEWS] = app_info["reviews"]
             apk[URL] = app_info["url"]
+            last_updated = app_info["updated"]
+            last_updated_date = datetime.datetime.fromtimestamp(last_updated)
+            apk["last_update"] = last_updated
+            apk["last_update_str"] = last_updated_date.strftime("%Y-%m-%d")
             cont += 1
         except:
             pass
     print("\nNumber of apps that exist in playstore: {}".format(cont))
 
 
+def select_apks(apks: list[dict]) -> None:
+    print("Selecting apks ...")
+
+    # criteria_labels = ["category", "min_sdk", "target_sdk", "updated", "classes", "activities", "methods"]
+    # criteria_weights = normalize_to_one([8, 2, 5, 8, 9, 10, 10]).tolist()
+    # # criteria_weights = [0.05, 0.05, 0.15, 0.2, 0.15, 0.2, 0.2]
+    # criteria_signals = [MAX, MAX, MAX, MAX, MAX, MAX, MAX]
+    criteria_labels = [DOWNLOADS, "last_update"]
+    criteria_weights = normalize_to_one([4, 6]).tolist()
+    criteria_signals = [MAX, MAX]
+
+    data = []
+    names = []
+    apk_by_name = {}
+    for apk in apks:
+        if apk[PLAYSTORE]: # TODO
+            data_item = [apk[DOWNLOADS], apk["last_update"]]
+            data.append(data_item)
+            apk_name = apk[FILENAME]
+            names.append(apk_name)
+            apk_by_name[apk_name] = apk
+
+    topsis = TOPSIS()
+    topsis.dataframe(data, names, criteria_labels)
+    topsis.set_weights_manually(criteria_weights)
+    topsis.set_signals(criteria_signals)
+    # topsis.decide()
+    topsis.decide(EnhancedAccuracy_)
+    aaa = topsis.df_decision
+    df = aaa.sort_values(by=["rank"], ascending=True, ignore_index=True)
+    result = df  #.head(10)
+    # print(result)
+    for ind in result.index:
+        apk_name = df['alternatives'][ind]
+        apk = apk_by_name[apk_name]
+        apk["selection_rank"] = df['rank'][ind]
+        apk["selection_score"] = df['performance score'][ind]
+        # print("{} ({}) = {}".format(df['rank'][ind], df['performance score'][ind], apk))
+
+
 def save_results(apks: list[dict], out: str) -> None:
     print("Saving the results ... ")
-    headers: list[str] = [FILENAME, NAME, APP_ID, FDROID, PLAYSTORE, DOWNLOADS, SCORE, RATINGS, REVIEWS, 'updated', URL]
+
+    keys = list(apks[0].keys())
     with open(out, "w") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer = csv.DictWriter(csvfile, fieldnames=keys)
         writer.writeheader()
         writer.writerows(apks)
+
+    # selected_keys: list[str] = [FILENAME, NAME, APP_ID, FDROID, PLAYSTORE, DOWNLOADS, SCORE, RATINGS, REVIEWS,
+    #                             "last_update_str", URL]
+    # with open(out, "w") as csvfile:
+    #     writer = csv.writer(csvfile)
+    #     writer.writerow(selected_keys)
+    #     for item in apks:
+    #         writer.writerow([item[key] for key in selected_keys])
+    #
+    # # #ValueError: dict contains fields not in fieldnames: 'apk_date', 'last_update', 'classes', 'summary', 'method_cov',
+    # # # 'source', 'apk_date_str', 'package', 'activities', 'min_sdk', 'methods', 'categories', 'rvsec_errors', 'selection_score', 'last_update_str', 'act_cov', 'target_sdk'
+    # # selected_keys: list[str] = [FILENAME, NAME, APP_ID, FDROID, PLAYSTORE, DOWNLOADS, SCORE, RATINGS, REVIEWS, 'updated', URL]
+    # # with open(out, "w") as csvfile:
+    # #     writer = csv.writer(csvfile)
+    # #     writer.writerow(selected_keys)
+    # #     writer.writerow([apks[key] for key in selected_keys])
+    # # # with open(out, "w") as csvfile:
+    # # #     writer = csv.DictWriter(csvfile)  #, fieldnames=headers)
+    # # #     # writer.writeheader()
+    # # #     writer.writerows(apks)
     print("Results saved in: {}".format(out))
+
+
+def get_app_id(apk_filename):
+    last_underscore_idx = apk_filename.rfind('_')
+    if last_underscore_idx == -1:
+        return apk_filename
+    else:
+        return apk_filename[:last_underscore_idx]
 
 
 def copy_apks_to_folder(out_file, output_directory, base_directory):
@@ -142,6 +271,10 @@ def find_and_copy_file(filename, output_directory, base_directory):
     walk_directory(base_directory)
 
 
+def normalize_to_one(arr):
+    return arr / np.sum(arr)
+
+
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     logging.getLogger("androguard").setLevel(logging.ERROR)
@@ -151,10 +284,10 @@ if __name__ == "__main__":
     results_file = os.path.join(base_dir, "final_results_analysis_jca.json")
     out_file = "/home/pedro/tmp/teste_playstore_novo.csv"
 
-    # execute(fdroid_file, results_file, out_file)
+    execute(fdroid_file, results_file, out_file)
 
-    base_directory = "/home/pedro/desenvolvimento/RV_ANDROID/apks"
-    output_directory = "/home/pedro/desenvolvimento/workspaces/workspaces-doutorado/workspace-rv/rvsec/rv-android/apks_experiment02/original"
-    copy_apks_to_folder(out_file, output_directory, base_directory)
+    # base_directory = "/home/pedro/desenvolvimento/RV_ANDROID/apks"
+    # output_directory = "/home/pedro/desenvolvimento/workspaces/workspaces-doutorado/workspace-rv/rvsec/rv-android/apks_experiment02/original"
+    # copy_apks_to_folder(out_file, output_directory, base_directory)
 
     print("FIM DE FESTA!!!")
